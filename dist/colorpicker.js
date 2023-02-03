3e5374a407583b1bc928adf79ef40fcac1b5caf9
Acero Liang Li <acero.liangli@stonybrook.edu>
2023-02-03 15:09:57 -0500

    third slider added

--- dist/colorpicker.js
+++ dist/colorpicker.js
@@ -54,6 +54,88 @@ redThumb.onmousedown = function (event) {
 redThumb.ondragstart = function () {
     return false;
 };
+var greenThumb = document.getElementById("greenThumb");
+greenThumb.onmousedown = function (event) {
+    // Slider range constraints 
+    let sliderY = greenThumb.getBoundingClientRect().top; // the y-cor of the slider thumb
+    let thumbLeftBound = $(".sliderTrack").get(0).getBoundingClientRect().left + (redThumb.offsetWidth / 2);
+    let thumbRightBound = thumbLeftBound + 180; // the right bound is the left bound plus size of slider
+    // shifting of the thumb
+    let shiftX = event.clientX - greenThumb.getBoundingClientRect().left;
+    greenThumb.style.position = 'absolute';
+    greenThumb.style.zIndex = 1000;
+    // move it from any existing parents directly to the body
+    // to position it relative to the body
+    document.body.append(greenThumb);
+    // and put this absolutely positioned text under the pointer
+    moveAt(event.pageX, sliderY);
+    // centers the text on the coordinates (pageX, pageY)
+    function moveAt(pageX, yCor) {
+        if (pageX < thumbLeftBound) {
+            pageX = thumbLeftBound;
+        }
+        if (pageX > thumbRightBound) {
+            pageX = thumbRightBound;
+        }
+        greenThumb.style.left = pageX - shiftX + 'px';
+        greenThumb.style.top = yCor + 'px';
+        updateSliderVal("green_value", pageX, thumbLeftBound, thumbRightBound); // update the pcolor value 
+    }
+    function onMouseMove(event) {
+        moveAt(event.pageX, sliderY);
+    }
+    //  move the text on mousemove
+    document.addEventListener('mousemove', onMouseMove);
+    // drop the text, remove unneeded handlers
+    document.onmouseup = function () {
+        document.removeEventListener('mousemove', onMouseMove);
+        greenThumb.onmouseup = null;
+    };
+};
+greenThumb.ondragstart = function () {
+    return false;
+};
+var blueThumb = document.getElementById("blueThumb");
+blueThumb.onmousedown = function (event) {
+    // Slider range constraints 
+    let sliderY = blueThumb.getBoundingClientRect().top; // the y-cor of the slider thumb
+    let thumbLeftBound = $(".sliderTrack").get(0).getBoundingClientRect().left + (redThumb.offsetWidth / 2);
+    let thumbRightBound = thumbLeftBound + 180; // the right bound is the left bound plus size of slider
+    // shifting of the thumb
+    let shiftX = event.clientX - blueThumb.getBoundingClientRect().left;
+    blueThumb.style.position = 'absolute';
+    blueThumb.style.zIndex = 1000;
+    // move it from any existing parents directly to the body
+    // to position it relative to the body
+    document.body.append(blueThumb);
+    // and put this absolutely positioned text under the pointer
+    moveAt(event.pageX, sliderY);
+    // centers the text on the coordinates (pageX, pageY)
+    function moveAt(pageX, yCor) {
+        if (pageX < thumbLeftBound) {
+            pageX = thumbLeftBound;
+        }
+        if (pageX > thumbRightBound) {
+            pageX = thumbRightBound;
+        }
+        blueThumb.style.left = pageX - shiftX + 'px';
+        blueThumb.style.top = yCor + 'px';
+        updateSliderVal("blue_value", pageX, thumbLeftBound, thumbRightBound); // update the pcolor value 
+    }
+    function onMouseMove(event) {
+        moveAt(event.pageX, sliderY);
+    }
+    //  move the text on mousemove
+    document.addEventListener('mousemove', onMouseMove);
+    // drop the text, remove unneeded handlers
+    document.onmouseup = function () {
+        document.removeEventListener('mousemove', onMouseMove);
+        blueThumb.onmouseup = null;
+    };
+};
+blueThumb.ondragstart = function () {
+    return false;
+};
 // SETUP
 let body = document.querySelector("body");
 body.onselectstart = function () {
--- src/colorpicker.ts
+++ src/colorpicker.ts
@@ -59,6 +59,98 @@ redThumb.ondragstart = function() : boolean{
     return false;
 };
 
+var greenThumb:any = document.getElementById("greenThumb")
+
+greenThumb.onmousedown = function(event:any) { 
+    // Slider range constraints 
+    let sliderY: number = greenThumb.getBoundingClientRect().top; // the y-cor of the slider thumb
+
+    let thumbLeftBound: number = $(".sliderTrack").get(0)!.getBoundingClientRect().left + (redThumb.offsetWidth / 2);
+    let thumbRightBound = thumbLeftBound + 180;// the right bound is the left bound plus size of slider
+    // shifting of the thumb
+    let shiftX = event.clientX - greenThumb.getBoundingClientRect().left;
+    greenThumb.style.position = 'absolute';
+    greenThumb.style.zIndex = 1000;
+    // move it from any existing parents directly to the body
+    // to position it relative to the body
+    document.body.append(greenThumb);
+    // and put this absolutely positioned text under the pointer
+    moveAt(event.pageX, sliderY);
+    // centers the text on the coordinates (pageX, pageY)
+    function moveAt(pageX:number, yCor:number) {
+        if(pageX < thumbLeftBound) {
+            pageX = thumbLeftBound;
+        }
+        if(pageX > thumbRightBound) {
+            pageX = thumbRightBound;
+        }
+      greenThumb.style.left = pageX - shiftX + 'px';
+      greenThumb.style.top = yCor + 'px';
+      updateSliderVal("green_value", pageX, thumbLeftBound, thumbRightBound)// update the pcolor value 
+
+    }
+    function onMouseMove(event:any) {
+      moveAt(event.pageX, sliderY);
+    }
+    //  move the text on mousemove
+    document.addEventListener('mousemove', onMouseMove);
+    // drop the text, remove unneeded handlers
+    document.onmouseup = function() {
+      document.removeEventListener('mousemove', onMouseMove);
+      greenThumb.onmouseup = null;
+    };
+  };
+
+greenThumb.ondragstart = function() : boolean{
+    return false;
+};
+
+var blueThumb:any = document.getElementById("blueThumb")
+
+blueThumb.onmousedown = function(event:any) { 
+    // Slider range constraints 
+    let sliderY: number = blueThumb.getBoundingClientRect().top; // the y-cor of the slider thumb
+
+    let thumbLeftBound: number = $(".sliderTrack").get(0)!.getBoundingClientRect().left + (redThumb.offsetWidth / 2);
+    let thumbRightBound = thumbLeftBound + 180;// the right bound is the left bound plus size of slider
+    // shifting of the thumb
+    let shiftX = event.clientX - blueThumb.getBoundingClientRect().left;
+    blueThumb.style.position = 'absolute';
+    blueThumb.style.zIndex = 1000;
+    // move it from any existing parents directly to the body
+    // to position it relative to the body
+    document.body.append(blueThumb);
+    // and put this absolutely positioned text under the pointer
+    moveAt(event.pageX, sliderY);
+    // centers the text on the coordinates (pageX, pageY)
+    function moveAt(pageX:number, yCor:number) {
+        if(pageX < thumbLeftBound) {
+            pageX = thumbLeftBound;
+        }
+        if(pageX > thumbRightBound) {
+            pageX = thumbRightBound;
+        }
+      blueThumb.style.left = pageX - shiftX + 'px';
+      blueThumb.style.top = yCor + 'px';
+      updateSliderVal("blue_value", pageX, thumbLeftBound, thumbRightBound)// update the pcolor value 
+
+    }
+    function onMouseMove(event:any) {
+      moveAt(event.pageX, sliderY);
+    }
+    //  move the text on mousemove
+    document.addEventListener('mousemove', onMouseMove);
+    // drop the text, remove unneeded handlers
+    document.onmouseup = function() {
+      document.removeEventListener('mousemove', onMouseMove);
+      blueThumb.onmouseup = null;
+    };
+  };
+
+blueThumb.ondragstart = function() : boolean{
+    return false;
+};
+
 // SETUP
 let body = document.querySelector("body");
 body!.onselectstart = function() : boolean {
